ec0307ed7fc6e9c4305ecd9973c9a2faba958893
tranthanhdat_01 <dat720606@gmail.com>
2025-06-07 11:51:41 +0700

    Add files via upload

---
+++ Services/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+
+
+namespace Services
+{
+    internal class IProductService
+    {
+        void SaveProduct(Product p);
+        void DeleteProduct(Product p);
+        void UpdateProduct(Product p);
+        List<Product> GetProducts();
+        Product GetProductById();
+
+    }
+}
